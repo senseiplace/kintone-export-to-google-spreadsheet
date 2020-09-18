ae21689a4c0e0507389f6ee52e464684412a9098
ハトネコエ hatonekoe@gmail.com <hato.nekoe@gmail.com>
2020-09-18 14:14:18 +0900

    プラグイン設定で必要なパラメーターを設定できるように

--- src/js/config.ts
+++ src/js/config.ts
@@ -6,22 +6,47 @@ const PLUGIN_ID = kintone.$PLUGIN_ID;
 
 const form = document.querySelector(".js-submit-settings")!;
 const cancelButton = document.querySelector(".js-cancel-button")!;
+const spreadsheetIdInput = document.querySelector<HTMLInputElement>(
+  ".js-text-spreadsheet-id"
+)!;
+const serviceAccountClientEmailInput = document.querySelector<HTMLInputElement>(
+  ".js-text-service-account-client-email"
+)!;
+const serviceAccountPrivateKeyInput = document.querySelector<HTMLInputElement>(
+  ".js-text-service-account-private-key"
+)!;
 const buttonLabelInput = document.querySelector<HTMLInputElement>(
   ".js-text-button-label"
 )!;
 const config = kintone.plugin.app.getConfig(PLUGIN_ID);
 
-if (config.buttonLabel) {
-  buttonLabelInput.value = config.buttonLabel;
+function initFieldValues() {
+  spreadsheetIdInput.value = config.spreadsheetId || "";
+  serviceAccountClientEmailInput.value = config.serviceAccountClientEmail || "";
+  serviceAccountPrivateKeyInput.value = config.serviceAccountPrivateKey || "";
+  buttonLabelInput.value = config.buttonLabel || "";
 }
+
+initFieldValues();
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
-  kintone.plugin.app.setConfig({ buttonLabel: buttonLabelInput.value }, () => {
-    alert("プラグイン設定の更新を完了するには「アプリを更新」を押してください");
-    window.location.href = "../../flow?app=" + kintone.app.getId();
-  });
+  kintone.plugin.app.setConfig(
+    {
+      spreadsheetId: spreadsheetIdInput.value,
+      serviceAccountClientEmail: serviceAccountClientEmailInput.value,
+      serviceAccountPrivateKey: serviceAccountPrivateKeyInput.value,
+      buttonLabel: buttonLabelInput.value,
+    },
+    () => {
+      alert(
+        "プラグイン設定の更新を完了するには「アプリを更新」を押してください"
+      );
+      window.location.href = "../../flow?app=" + kintone.app.getId();
+    }
+  );
 });
+
 cancelButton.addEventListener("click", () => {
   window.location.href = "../../" + kintone.app.getId() + "/plugin/";
 });
